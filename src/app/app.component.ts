a7c90c463282213d6832fc3ae0a1a8844979faab
jamesjf7 <jamesjfoong2000@gmail.com>
2022-08-31 01:05:17 +0700

    update home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { History, Time } from 'src/types/data-types';
+// import Cube from 'cube-simulator';
+
+// const cube = new Cube();
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'rubic-simulator';
+  constructor() {}
+
+  title: string = 'rubic-simulator';
+  scramble: string = '-';
+
+  time: Time = {
+    timems: 0,
+    timer: null,
+    running: false,
+  };
+
+  histories: History[] = [];
+
+  @HostListener('document:keyup', ['$event'])
+  handleKeyUpEvent(event: KeyboardEvent) {
+    switch (event.key) {
+      case ' ':
+        if (this.time.running) {
+          this.time.timems = 0;
+          const startTime = Date.now() - (this.time.timems || 0);
+          this.time.timer = setInterval(() => {
+            this.time.timems = Date.now() - startTime;
+          });
+        } else {
+          if (this.time.timer) {
+            clearInterval(this.time.timer);
+            this.histories.push({
+              time: this.time.timems,
+              scramble: this.scramble,
+            });
+          }
+        }
+        this.time.running = !this.time.running;
+        break;
+      default:
+        console.log(event.key);
+        break;
+    }
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyDownEvent(event: KeyboardEvent) {
+    switch (event.key) {
+      case ' ':
+        break;
+      default:
+        break;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimerComponent } from './timer/timer.component';
+import { HistoryComponent } from './history/history.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TimerComponent, HistoryComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
